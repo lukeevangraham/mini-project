cb4a5002357f726cf6292a219a294a994f96c2e4
Cefalu Wave <luke.evan.graham@gmail.com>
2020-12-14 22:59:16 -0800

    working on sticky nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,20 @@
-// import Flickety from "./flickity.pkgd.min.js";
-let Flickity = require("flickity")
+// FOR THE STICKY NAV
+// document.querySelector(".js--section-intro").waypoint(
+//   function (direction) {
+//     if (direction == "down") {
+//       console.log("DOWN");
+//     } else {
+//       console.log("UP");
+//     }
+//   },
+//   {
+//     offset: "60px;",
+//   }
+// );
 
+// import Flickety from "./flickity.pkgd.min.js";
+let Flickity = require("flickity");
+import "./waypoints/noframework.waypoints.min.js";
 
 var elem = document.querySelector(".main-carousel");
 var flkty = new Flickity(elem, {
@@ -8,7 +22,57 @@ var flkty = new Flickity(elem, {
   cellAlign: "left",
   contain: true,
   freeScroll: true,
-  arrowShape: "M99.44,52.05v-4.09h-89.4L40.8,22.49l-2.64-3.12L1.39,49.92l36.77,30.73l2.63-3.13L10.04,52.05 C10.04,52.05,99.44,52.05,99.44,52.05z"
+  arrowShape:
+    "M99.44,52.05v-4.09h-89.4L40.8,22.49l-2.64-3.12L1.39,49.92l36.77,30.73l2.63-3.13L10.04,52.05 C10.04,52.05,99.44,52.05,99.44,52.05z",
 });
 
-var flkty = new Flickity( '.main-carousel', {})
+var flkty = new Flickity(".main-carousel", {});
+
+// MOBILE NAV
+document.querySelector(".js--nav-icon").addEventListener("click", (e) => {
+  e.preventDefault();
+  let nav = document.querySelector(".js--main-nav");
+  let icon = document.querySelector(".js--nav-icon");
+
+  if (!nav.classList.contains("active")) {
+    nav.classList.add("active");
+    nav.style.height = "auto";
+
+    let height = nav.clientHeight + "px";
+
+    nav.style.height = "0px";
+
+    setTimeout(() => {
+      nav.style.height = height;
+    }, 0);
+  } else {
+    nav.style.height = "0px";
+
+    nav.addEventListener(
+      "transitionend",
+      function () {
+        nav.classList.remove("active");
+      },
+      {
+        once: true,
+      }
+    );
+  }
+  if (icon.src.includes("/menu-outline.svg")) {
+    icon.src = "./img/close-outline.svg";
+  } else {
+    icon.src = "./img/menu-outline.svg";
+  }
+});
+// $('.js--nav-icon').click(function () {
+//   let nav = $('.js--main-nav');
+//   let icon = $('.js--nav-icon i')
+//   nav.slideToggle(200);
+//   if (icon.hasClass('ion-navicon-round')) {
+//       icon.addClass('ion-close-round')
+//       icon.removeClass('ion-navicon-round')
+//   } else {
+//       icon.addClass('ion-navicon-round')
+//       icon.removeClass('ion-close-round')
+//   }
+// })
